389a21f11b4b7a3891e33de97a4cc8bb8e280f6e
YunMin-Song <thd930308@naver.com>
2020-11-01 03:01:25 +0900

    [Yunmin] makeOnListener 정의

--- stores/stores.js
+++ stores/stores.js
@@ -31,6 +31,46 @@ let positions = [
         // image: '<img src="img/test1.png" alt = "My Image" width="50">',
         latlng: new kakao.maps.LatLng(36.809982, 127.111104)
     },
+    {
+        content: '<div class="infoWindows">' +
+            '<img src="img/img2.jpg" alt = "My Image" width="100px" height="100px" class="infoWindows-img">' +
+            '<div class="infoWindows-sub">'+
+            '<b class="infoWindows-sub-title">카페 하인츠 불당점</b>' +
+            '<span class="infoWindows-sub-description">' +
+            '커피 전문점 입니다.' +
+            '</span>' +
+            '<div class="infoWindows-Button">'+
+            '<input type=button style="width:50pt;height:20pt;" value="Open">' +
+            '<input type=button style="width:50pt;height:20pt;" value="Close">' +
+            // 아래 버튼에 예약 페이지 링크 첨부 하시면 됩니다
+            '<input type="button" style="width:70pt;height:20pt;" value="예약">' +
+            '</div>'+
+            '</div>'+
+            '</div>',
+        removable: iwRemoveable,
+        // image: '<img src="img/test1.png" alt = "My Image" width="50">',
+        latlng: new kakao.maps.LatLng(36.809762, 127.110536)
+    },
+    {
+        content: '<div class="infoWindows">' +
+            '<img src="img/img3.jpg" alt = "My Image" width="100px" height="100px" class="infoWindows-img">' +
+            '<div class="infoWindows-sub">'+
+            '<b class="infoWindows-sub-title">신불당 할리스커피</b>' +
+            '<span class="infoWindows-sub-description">' +
+            '다들 할리스 커피 좋아하시나요? 할리스 커피는 맛있습니다. 왜냐하면 맛있거든요.' +
+            '</span>' +
+            '<div class="infoWindows-Button">'+
+            '<input type=button style="width:50pt;height:20pt;" value="Open">' +
+            '<input type=button style="width:50pt;height:20pt;" value="Close">' +
+            // 아래 버튼에 예약 페이지 링크 첨부 하시면 됩니다
+            '<input type="button" style="width:70pt;height:20pt;" value="예약">' +
+            '</div>'+
+            '</div>'+
+            '</div>',
+        removable: iwRemoveable,
+        // image: '<img src="img/test1.png" alt = "My Image" width="50">',
+        latlng: new kakao.maps.LatLng(36.810715, 127.115145)
+    },
 ];
 
 /***********************마커 및 인포윈도우 생성***************************/
@@ -39,7 +79,6 @@ for (var i = 0; i < positions.length; i ++) {
     var marker = new kakao.maps.Marker({
         map: map, // 마커를 표시할 지도
         position: positions[i].latlng, // 마커의 위치
-        clickable: true,
     });
 
     let infowindow = new kakao.maps.InfoWindow({
@@ -47,8 +86,21 @@ for (var i = 0; i < positions.length; i ++) {
         removable: iwRemoveable,
     });
 
-    kakao.maps.event.addListener(marker, 'click', function() {
-        // 마커 위에 인포윈도우를 표시합니다
+    kakao.maps.event.addListener(marker, 'click', makeOnListener(map ,marker ,infowindow));
+        // // 마커 위에 인포윈도우를 표시합니다
+        // infowindow.open(map, marker);
+}
+
+// 인포윈도우를 표시하는 클로저를 만드는 함수입니다
+function makeOnListener(map, marker, infowindow) {
+    return function() {
         infowindow.open(map, marker);
-    });
+    };
+}
+
+// 인포윈도우를 닫는 클로저를 만드는 함수입니다
+function makeOutListener(infowindow) {
+    return function() {
+        infowindow.close();
+    };
 }
